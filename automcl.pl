27c8da838c8b7c9a85a916c338a59fc9366b7704
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2012-01-11 09:20:30 -0500

    Finished adjust_fasta stage

--- automcl.pl
+++ automcl.pl
@@ -9,6 +9,7 @@ use lib $FindBin::Bin;
 use AutoConfig;
 
 use Getopt::Long;
+use Cwd qw(getcwd abs_path);
 use File::Basename qw(basename dirname);
 
 my $script_dir = $FindBin::Bin;
@@ -30,6 +31,31 @@ sub check_dependencies
 	die "Error: orthomclAdjustFasta does not exist in \"$orthomclbin\"" if (not -e "$orthomclbin/orthomclAdjustFasta");
 }
 
+sub adjust_fasta
+{
+	my ($input_dir, $output) = @_;
+
+	my $ortho_adjust_fasta = $AutoConfig::params{'orthomcl'}{'bin'}.'/orthomclAdjustFasta';
+
+	opendir(my $dh, $input_dir) or die "Could not open directory $input_dir";
+
+	my $cwd = getcwd;
+	chdir $output or die "Could not change to directory $output";
+
+	my $file = readdir($dh);
+	while (defined $file)
+	{
+		my $base = basename($file, ('.faa','.fasta'));
+
+		system("$ortho_adjust_fasta $base \"$input_dir/$file\" 1") == 0 or die "Error running orthomclAdjustFasta for file $file";
+
+		$file = readdir($dh);
+	}
+	closedir($dh);
+
+	chdir $cwd;
+}
+
 my ($input_dir, $output_dir);
 
 if (!GetOptions(
@@ -45,5 +71,25 @@ die "Error: no input-dir defined\n".usage if (not defined $input_dir);
 die "Error: input-dir not a directory\n".usage if (not -d $input_dir);
 die "Error: output-dir not defined\n".usage if (not defined $output_dir);
 
+$input_dir = abs_path($input_dir);
+$output_dir = abs_path($output_dir);
 
+if (-e $output_dir)
+{
+    print "Warning: directory \"$output_dir\" already exists, are you sure you want to store data here [Y]? ";
+    my $response = <>;
+    chomp $response;
+    if (not ($response eq 'y' or $response eq 'Y' or $response eq ''))
+    {
+        die "Directory \"$output_dir\" already exists, could not continue.";
+    }
+}
+else
+{
+	mkdir $output_dir;
+}
 
+my $compliant_dir = "$output_dir/compliant_fasta";
+mkdir $compliant_dir if (not -e $compliant_dir);
+
+adjust_fasta($input_dir,$compliant_dir);
